51841890c491161551fc219c36dfd6aa5fb8c722
Recruitly <siva@recruitly.io>
2024-12-24 16:53:45 +0530

    tasks grid in progress

--- src/components/userpriority/drilldown/PlannerDrillDownData.jsx
+++ src/components/userpriority/drilldown/PlannerDrillDownData.jsx
@@ -1,58 +1,73 @@
 import { Layout, Menu } from "antd";
 import { CgWebsite } from "react-icons/cg";
 import { FaTasks } from "react-icons/fa";
+import { HashRouter as Router, Link, Route, Routes } from "react-router-dom";
 
 import { CalendarOutlined } from "@ant-design/icons";
 import PlannerGridTasks from "@components/userpriority/drilldown/PlannerTasksGrid.jsx";
 import { Handshake, Microphone } from "@phosphor-icons/react";
+import { useState } from "react";
 
 const { Sider } = Layout;
 
 const PlannerDrillDownData = ({type="Today",filterType="ALL"}) => {
 
-  return (
+  const [selectedKey, setSelectedKey] = useState("/tasks");
+
+  const handleMenuClick = (e) => {
+    setSelectedKey(e.key); // Navigate to the route
+  };
+
+  return (<Router>
       <Layout>
         <Sider width={200} style={{background :"#ffffff",height:"100%"}}>
           <Menu
             mode="inline"
-            defaultSelectedKeys={['1']}
+            selectedKey={selectedKey}
             style={{ borderRight: 0 }}
+            onClick={handleMenuClick}
             items={[
               {
-                key: '1',
+                key: 'tasks',
                 icon: <FaTasks />,
-                label: 'Tasks/Reminders',
+                label: <Link to={"/tasks"}>Tasks/Reminders</Link>,
               },
               {
-                key: '2',
+                key: 'job_applications',
                 icon: <CgWebsite />,
-                label: 'Job Applications',
+                label: <Link to={"/job_applications"}>Job Applications</Link>,
               },
               {
-                key: '3',
+                key: 'events',
                 icon: <CalendarOutlined />,
-                label: 'Calendar Events',
+                label: <Link to={"/events"}>Calendar Events</Link>,
               },
               {
-                key: '4',
+                key: 'interviews',
                 icon: <Microphone />,
-                label: 'Interviews',
+                label: <Link to={"/interviews"}>Interviews</Link>,
               },
               {
-                key: '5',
+                key: 'placement_starters',
                 icon: <Handshake />,
-                label: 'Placement starters',
+                label: <Link to={"/placement_starters"}>Placement Starters</Link>,
               },
             ]}
           />
         </Sider>
-        <Layout style={{ padding: '0 24px 24px',height: "80vh" }}>
+        <Layout style={{height: "80vh" }}>
 
-          <PlannerGridTasks/>
+          <Routes>
+            <Route path={"/tasks"} element={<PlannerGridTasks/>}/>
+            <Route path={"/job_applications"} element={<div>Job Applications</div>}/>
+            <Route path={"/events"} element={<div>Events</div>}/>
+            <Route path={"/interviews"} element={<div>Interviews</div>}/>
+            <Route path={"/placement_starters"} element={<div>Placement starters</div>}/>
+          </Routes>
 
         </Layout>
       </Layout>
-  );
+  </Router>);
 }
 
 export default PlannerDrillDownData;
