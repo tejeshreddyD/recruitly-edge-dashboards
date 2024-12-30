f9831066718d5ece265ca0124c09c6fb365a66c8
siva <siva@recruitly.io>
2024-12-30 10:50:09 +0530

    view calendar event from planner

--- src/utils/actionsUtil.js
+++ src/utils/actionsUtil.js
@@ -10,7 +10,7 @@ export const dashboardAction = (e, code, paramObj) => {
 
   return new Promise((resolve, reject) => {
     if (window.EDGE_UTIL && typeof window.EDGE_UTIL.dashboardAction === 'function') {
-      window.EDGE_UTIL.dashboardAction({code, paramObj}).then(resolve).catch(reject);
+      window.EDGE_UTIL.dashboardAction({actionCode:code, paramsObj:paramObj}).then(resolve).catch(reject);
     } else {
       reject(new Error("dashboardAction is not available on EDGE_UTIL"));
     }
