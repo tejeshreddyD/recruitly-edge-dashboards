5a5f1cddbfcb5a983c70767bf74288553b791501
siva <siva@recruitly.io>
2024-12-31 22:52:56 +0530

    user job forecast in progress

--- src/components/jobs/JobsForecastGrid.jsx
+++ src/components/jobs/JobsForecastGrid.jsx
@@ -12,6 +12,8 @@ import { AgGridReact } from "ag-grid-react";
 import { RECRUITLY_AGGRID_THEME } from "@constants";
 import useUserDashboardJobsStore from "@api/userDashboardJobsStore.js";
 import { getDateStringByUserTimeZone } from "@utils/dateUtil.js";
+import { Flex, Tooltip } from "antd";
+import { SmileOutlined } from "@ant-design/icons";
 ModuleRegistry.registerModules([
   ClientSideRowModelModule,
   ValidationModule
@@ -31,10 +33,15 @@ const JobForecastGrid = ({ statuses = [] }) => {
   statuses.forEach((status) => {
     status_list.push({
       headerName: status.name,
-      field: status.statusCode,
-      width:100
+      field: `pipelines['${status.statusCode}']`,
+      width:100,
+      cellRenderer: (row) => {
+        return row.data.pipelines[status.statusCode];
+      }
     });
   });
+
+  console.log(status_list);
 
   const defaultColDef = useMemo(() => {
     return {
@@ -50,7 +57,17 @@ const JobForecastGrid = ({ statuses = [] }) => {
       headerName: "Job",
       field: "jobLabel",
       minWidth: 250,
-      pinned:'left'
+      pinned:'left',
+      cellRenderer: (row) => {
+
+        return (<><Flex justify="start" align={"center"}>
+          <Tooltip title={"Perform search"}><SmileOutlined size={35}/></Tooltip>
+          <Flex  vertical align="flex-end" justify="start">
+            {row.data.jobLabel}
+          </Flex>
+        </Flex></>)
+
+      }
     },
     {
       headerName: "Pulse",
@@ -66,7 +83,7 @@ const JobForecastGrid = ({ statuses = [] }) => {
       }
     },
     {
-      headerName: "Closing Date",
+      headerName: "Est.Closing Date",
       field: "closingDate",
       minWidth: 150,
       cellRenderer: (param) => {
@@ -78,20 +95,7 @@ const JobForecastGrid = ({ statuses = [] }) => {
       field: "fee",
       minWidth: 150,
     },
-    {
-      headerName: "Pipeline Count",
-      children: status_list,
-    },
-    {
-      headerName: "Weighted Pipeline",
-      children: [
-        { headerName: "Current", field: "current" },
-        { headerName: "30 days", field: "gold" },
-        { headerName: "60 days", field: "silver" },
-        { headerName: "90 days", field: "bronze" },
-        { headerName: "120 days", field: "bronze" },
-      ],
-    },
+    ...status_list
   ]);
 
   const onGridReady = useCallback((params) => {
